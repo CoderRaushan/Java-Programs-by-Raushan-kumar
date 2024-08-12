4715fe40f4e88a1f22e910da642e59d5aafa7c21
CoderRaushanSingh <kumarraushan7631867534@gmail.com>
2024-08-12 14:04:49 +0530

    java

---
+++ Lab/doLoop.java
@@ -0,0 +1,12 @@
+public class doLoop
+{
+    public static void main(String[] args) {
+
+         int n=2;
+        do
+        {
+            System.out.println(n);
+            n=n+2;
+        }while(n<=20);
+    }
+}
---
+++ Lab/do_while_loop.java
@@ -0,0 +1,11 @@
+public class do_while_loop {
+   public static void main(String[] args) 
+   {
+    int n=2;
+    while(n<=20)
+    {
+        System.out.println(n);
+        n+=2;
+    }
+   }
+}
