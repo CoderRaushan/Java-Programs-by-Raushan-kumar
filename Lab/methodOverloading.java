e88694470644f7b58866452e3a9378b931507f8f
CoderRaushanSingh <kumarraushan7631867534@gmail.com>
2024-08-12 13:52:05 +0530

    method overloaingnn

---
+++ Lab/methodOverloading.java
@@ -0,0 +1,24 @@
+public class methodOverloading 
+{
+    public static int add(int x,int y) 
+    {
+        return x+y;
+    }
+    public static double add(double  x,double  y) 
+    {
+        return x+y;
+    }
+    public static String  add(String  x,String  y) 
+    {
+        return x+y;
+    }
+public static void main(String[] args)
+{
+  int int_ans=add(4,4);
+  double double_ans=add(1.2,2.2);
+  String name=add("Raushan ","Kumar");
+  System.out.println(int_ans);
+  System.out.println(double_ans);
+  System.out.println(name);
+}
+}
