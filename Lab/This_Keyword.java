8cae67b98783761453b0131b91da0ceed95f6df9
CoderRaushan <raushankumar23082004@gmail.com>
2024-09-18 21:45:28 +0530

    update

--- Lab/This_Keyword.java
+++ Lab/This_Keyword.java
@@ -1,11 +1,28 @@
+// public class This_Keyword {
+//     void fun()
+//     {
+//         System.out.println(this);
+//     }
+//     public static void main(String[] args) 
+//     {
+//         This_Keyword t=new This_Keyword();
+//         t.fun(); //output will be : This_Keyword@4f2410ac refering to the current object
+//     }
+// }
+
+
 public class This_Keyword {
-    void fun()
+    void fun1()
     {
-        System.out.println(this);
+        System.out.println("hi i am fun1");
+    }
+    void fun2()
+    {
+       this.fun1();
     }
     public static void main(String[] args) 
     {
         This_Keyword t=new This_Keyword();
-        t.fun(); //output will be : This_Keyword@4f2410ac refering to the current object
+        t.fun2(); 
     }
 }
