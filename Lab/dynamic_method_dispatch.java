9625b37aca7869f8a769ae81af030edc9e8dcad2
CoderRaushan <raushankumar23082004@gmail.com>
2024-09-26 10:50:31 +0530

    java pro innern classs

--- Lab/dynamic_method_dispatch.java
+++ Lab/dynamic_method_dispatch.java
@@ -1,19 +1,25 @@
 
-class Parent {
-    void show() {
+class Parent 
+{
+    void show() 
+    {
         System.out.println("Parent's show method");
     }
 }
 
-class Child extends Parent {
+class Child extends Parent 
+{
     @Override
-    void show() {
+    void show() 
+    {
         System.out.println("Child's show method");
     }
 }
 
-public class dynamic_method_dispatch {
-    public static void main(String[] args) {
+public class dynamic_method_dispatch 
+{
+    public static void main(String[] args) 
+    {
         Parent obj = new Child(); // Upcasting
         obj.show(); // Calls Child's show method
     }
