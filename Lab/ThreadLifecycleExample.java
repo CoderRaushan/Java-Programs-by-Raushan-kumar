937b6a023459568dc6c989410a19fe0b559588cd
CoderRaushan <raushankumar23082004@gmail.com>
2024-10-28 14:31:48 +0530

    java lab

--- Lab/ThreadLifecycleExample.java
+++ Lab/ThreadLifecycleExample.java
@@ -1,17 +1,18 @@
-class LifecycleThread extends Thread {
+class LifecycleThread extends Thread 
+{
     public void run() {
         System.out.println("Running: " + this.getName());
     }
 }
 
 public class ThreadLifecycleExample {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException 
+    {
         LifecycleThread t1 = new LifecycleThread();
         System.out.println("Thread State after creation: " + t1.getState()); 
         t1.start();
         System.out.println("Thread State after start: " + t1.getState()); 
-        
-        t1.join(); // Ensures main thread waits for t1 to complete
+        t1.join(); 
         System.out.println("Thread State after join: " + t1.getState()); 
     }
 }
