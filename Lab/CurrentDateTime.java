1aea29a769de1a9bdc82ec48d4d56ae0e7c3f6b5
CoderRaushan <raushankumar23082004@gmail.com>
2024-11-28 16:19:51 +0530

    java code lab

---
+++ Lab/CurrentDateTime.java
@@ -0,0 +1,8 @@
+import java.time.*;
+
+public class CurrentDateTime {
+    public static void main(String[] args) {
+        // Display the current date and time
+        System.out.println("Current Date and Time: " + LocalDateTime.now());
+    }
+}
